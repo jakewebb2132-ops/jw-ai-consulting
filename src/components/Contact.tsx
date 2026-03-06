6d07a2c445c41f1f2e64e226cbd91e4af5cc49ba
Jake Webb <jake@Protopia-Admins-MacBook-Air.local>
2026-03-06 07:50:17 -0600

    Fix: Update Formspree ID to xeerqaab

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -25,7 +25,7 @@ const Contact = () => {
         setError(null);
 
         try {
-            const response = await fetch("https://formspree.io/f/mqaeobon", {
+            const response = await fetch("https://formspree.io/f/xeerqaab", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
